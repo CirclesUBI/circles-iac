c67e90f1ac7a284c0cb8953e81601819c4c8ea0f
Andreas Dzialocha <x12@adz.garden>
2021-06-23 13:46:34 +0200

    Also update common config for terraform

--- do-infra-setup/common/main.tf
+++ do-infra-setup/common/main.tf
@@ -4,10 +4,14 @@ terraform {
     key                         = "terraform.tfstate"
     bucket                      = "circles-common-tf-state"
     region                      = "us-west-1"
-    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
+  }
+
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
   }
 }
 
@@ -17,6 +21,7 @@ provider "digitalocean" {
 
 resource "digitalocean_container_registry" "primary" {
   name = "circles-registry"
+  subscription_tier_slug = "basic"
 }
 
 resource "digitalocean_container_registry_docker_credentials" "primary" {
