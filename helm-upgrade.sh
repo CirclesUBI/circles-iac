a6356bec4105549c986364568d23d25ff6da91a9
Andreas Dzialocha <x12@adz.garden>
2021-06-28 14:09:15 +0200

    Show kubectl context before running helm upgrade

--- helm-upgrade.sh
+++ helm-upgrade.sh
@@ -7,6 +7,8 @@ fi
 
 ENV=$1
 
+echo "Your current kubectl context is: $(kubectl config current-context)"
+
 read -p "Are you sure you want to upgrade? (Yy) " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
