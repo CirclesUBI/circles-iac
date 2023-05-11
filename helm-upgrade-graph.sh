7af32b9fe993fc4b3ab949020760ea579975ba50
JacqueGM <55389166+JacqueGM@users.noreply.github.com>
2023-05-11 11:14:03 +0200

    Graph chiado (#92)
    * update group vars for chiado
    
    * add graph for chiado using public node
    
    * updated values.yaml
    
    * update subgraph to release candidate

--- helm-upgrade-graph.sh
+++ helm-upgrade-graph.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Your current kubectl context is: $(kubectl config current-context)"
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage ./helm-upgrade.sh <prd|stg>"
+  exit 0
+fi
+
+ENV=$1
 
 read -p "Are you sure you want to upgrade? (Yy) " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -9,4 +15,4 @@ then
 fi
 echo
 
-helm upgrade --install -f ./helm/circles-graphprotocol/values.yaml graph-protocol  ./helm/circles-graphprotocol
+helm upgrade --install -f ./helm/circles-graphprotocol/values-$ENV.yaml  -n graph graph-protocol  ./helm/circles-graphprotocol
